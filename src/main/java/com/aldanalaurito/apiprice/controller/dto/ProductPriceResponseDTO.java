e9f392f54609c2537a91eda31cd66b809ec0949f
Aldana Laurito <aldana.laurito.AL@gmail.com>
2025-01-15 13:28:42 +0100

    Creted DTOs and Constants class

---
+++ src/main/java/com/aldanalaurito/apiprice/controller/dto/ProductPriceResponseDTO.java
@@ -0,0 +1,50 @@
+package com.aldanalaurito.apiprice.controller.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(name = "product-price-dto")
+public class ProductPriceResponseDTO {
+
+    long product;
+
+    int brand;
+
+    @JsonProperty("price_list")
+    Integer priceList;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd-HH:mm:ss")
+    @JsonProperty("start_date")
+    LocalDateTime startDate;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd-HH:mm:ss")
+    @JsonProperty("end_date")
+    LocalDateTime endDate;
+
+    BigDecimal price;
+
+    String currency;
+}
