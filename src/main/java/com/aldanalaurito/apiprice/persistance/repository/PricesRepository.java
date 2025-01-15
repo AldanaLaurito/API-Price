155483a32775ddcbd9b716e8ecb16723062af6b9
Aldana Laurito <aldana.laurito.AL@gmail.com>
2025-01-15 13:37:55 +0100

    Created Repository

---
+++ src/main/java/com/aldanalaurito/apiprice/persistance/repository/PricesRepository.java
@@ -0,0 +1,22 @@
+package com.aldanalaurito.apiprice.persistance.repository;
+
+import com.aldanalaurito.apiprice.persistance.entities.PriceEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface PricesRepository extends CrudRepository<PriceEntity, Long> {
+
+    @Query(value = "SELECT * FROM PRICES WHERE " +
+            "PRODUCT_ID = :productId AND " +
+            "BRAND_ID = :brandId AND " +
+            "START_DATE <= :dateTime AND " +
+            "END_DATE >= :dateTime " +
+            "ORDER BY PRIORITY DESC LIMIT 1",
+            nativeQuery = true)
+    PriceEntity findFirstByProductIdAndBrandIdAndDatetime(
+            Long productId, Integer brandId, LocalDateTime dateTime);
+}
